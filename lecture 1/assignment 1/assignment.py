ee1fbebe36b70b851cbdf7026fc0086af9d136d8
amar amer <september06@users.noreply.github.com>
2019-06-24 18:32:44 +0300

    first lesson in python

---
+++ lecture 1/assignment 1/assignment.py
@@ -0,0 +1,16 @@
+#PYTHON randint FUNCTION : randint(minimum number , maximum number) 
+#take 2 arguments 
+#Return arandom integer between a maximum and minimum number incloud
+import random
+print("""
+   _    _
+  | |  | |
+  | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+  |  __  |/ _` | '_ \\/  _ `| '_ ` _ \\ / _ `| '_ \\
+  | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+  |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                       __/ |
+                      |___/
+ """,random.randint(5,10))
+
+input("please press enter to exit")
---
+++ lecture 1/question.py
@@ -0,0 +1,104 @@
+#the Situations of the hangman :
+#first one :
+print("""
+    h==y====x
+    d y     
+    dy      
+    d
+    d
+    d
+    d 
+    d
+    d
+    d 
+ +++m+++ """)
+
+#second one :
+
+print("""
+    h==y====x
+    d y     |
+    dy      |
+    d
+    d
+    d
+    d
+    d
+    d
+    d
+ +++m+++ """)
+
+#picture 3:
+print("""
+    h==y====x
+    d y     |
+    dy      |
+    d       0
+    d
+    d
+    d
+    d
+    d
+    d 
+ +++m+++ """)
+
+#picture 4:
+print("""
+    h==y====x
+    d y     |
+    dy      |
+    d      _0_
+    d      |.|
+    d      | |
+    d
+    d
+    d
+    d
+ +++m+++ """)
+
+#picture 5:
+print("""
+    h==y====x
+    d y     |
+    dy      |
+    d      _0_
+    d     /|.|\\
+    d    / |_| \\
+    d
+    d
+    d
+    d
+ +++m+++ """)
+
+#picture 6:
+print("""
+    h==y====x
+    d y     |
+    dy      |
+    d      _0_
+    d     /|.|\\
+    d    / |_| \\
+    d     /  
+    d    _|
+    d
+    d
+ +++m+++ """)
+
+#picture 7:
+print("""
+    h==y====x
+    d y     |
+    dy      |
+    d      _0_
+    d     /|.|\\
+    d    / |_| \\
+    d     /   \\
+    d    _|   |_
+    d
+    d   
+ +++m+++ """)
+ 
+
+input("please press enter to exit")
+
+
