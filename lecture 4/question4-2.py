86a2a3d5dae948446557f390584ee248e094fc6d
amar amer <september06@users.noreply.github.com>
2019-07-21 12:10:53 +0300

    Update question4-2.py

--- lecture 4/question4-2.py
+++ lecture 4/question4-2.py
@@ -1,9 +1,9 @@
-str = input("Insert the temperature you would like to convert : ")
-f_or_c = str[-1]
-len_string = int(len(str))
-the_temperature = float(str[:-1])
+str1 = input("Insert the temperature you would like to convert : ")
+f_or_c = str1[-1]
+len_string = int(len(str1))
+the_temperature = float(str1[:-1])
 if (f_or_c == 'c') or (f_or_c == 'C'): 
-    print(((the_temperature * 9.0)+(32.0 *5.0))/5)	
+    print((str(((the_temperature * 9.0)+(32.0 *5.0))/5))+"f")	
 else:
-    print(((the_temperature*5.0)-160.0)/9.0)	
+    print((str(((the_temperature*5.0)-160.0)/9.0))+"c")	
 input("Insert the temperature you would like to convert :")
