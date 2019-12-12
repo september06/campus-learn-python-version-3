eac6be65f12d278e45d6327dc675d49b05f61bd1
amar amer <september06@users.noreply.github.com>
2019-12-12 23:19:27 +0200

    rolling task

--- ‏‏lecture 5/assignment 5/lec5.py
+++ ‏‏lecture 5/assignment 5/lec5.py
@@ -3,6 +3,24 @@
 #Return arandom integer between a maximum and minimum number incloud
 import random
 import re
+
+def is_valid_input(letter_guessed):
+#the second and thred condution
+    pattern = re.compile('[^A-Za-z]+')	
+    if(len(letter_guessed) > 1) and (pattern.search(letter_guessed)):
+        print('E3')
+        return 'False'
+#If the player entered a letter string containing more than one character, print the string "E1".
+    elif(len(letter_guessed) > 1):
+        print('E1')
+        return 'False'
+#If the player has entered a non-English character check by regular expression print the string "E2".
+    elif (pattern.match(letter_guessed)):
+        print("E2")
+        return 'False'
+    else:
+        print(letter_guessed.lower())
+        return 'True'
 HANGMAN_ASCII_ART = """
    _    _
   | |  | |
@@ -14,22 +32,10 @@ HANGMAN_ASCII_ART = """
                       |___/
  """
 MAX_TRIES=random.randint(5, 10)
-pattern = re.compile('[^A-Za-z]+')	
 print(HANGMAN_ASCII_ART, MAX_TRIES)
-
-
 letter_guessed_by_player = input("Please Guess a letter: ")
-#the second and thred condution
-if(len(letter_guessed_by_player) > 1) and (pattern.search(letter_guessed_by_player)):
-    print('E3')
-#If the player entered a letter string containing more than one character, print the string "E1".
-elif(len(letter_guessed_by_player) > 1):
-    print('E1')
-#If the player has entered a non-English character check by regular expression print the string "E2".
-elif (pattern.match(letter_guessed_by_player)):
-    print("E2")
-else:
-    print(letter_guessed_by_player.lower())
+legail = is_valid_input(letter_guessed_by_player)
+print(legail)
 word_typed_by_player = input("Please enter a word: ")
 print("_ " * len(word_typed_by_player))
 input("please press enter to exit")
