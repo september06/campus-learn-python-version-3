9cf5909d2f9ae351a51820127ab91ad2cb7ca2ba
amar amer <september06@users.noreply.github.com>
2019-12-21 00:44:50 +0200

    Sixth chapter in python

--- lecture 6/assignment 6/lec6.py
+++ lecture 6/assignment 6/lec6.py
@@ -4,23 +4,37 @@
 import random
 import re
 
-def is_valid_input(letter_guessed):
+def check_valid_input(letter_guessed, old_letters_guessed):
 #the second and thred condution
     pattern = re.compile('[^A-Za-z]+')	
     if(len(letter_guessed) > 1) and (pattern.search(letter_guessed)):
         print('E3')
-        return 'False'
+        return False
 #If the player entered a letter string containing more than one character, print the string "E1".
     elif(len(letter_guessed) > 1):
         print('E1')
-        return 'False'
+        return False
 #If the player has entered a non-English character check by regular expression print the string "E2".
     elif (pattern.match(letter_guessed)):
         print("E2")
-        return 'False'
+        return False
+    elif (letter_guessed in old_letters_guessed):
+        return False
     else:
         print(letter_guessed.lower())
-        return 'True'
+        return True
+		
+		
+def try_update_letter_guessed(letter_guessed, old_letters_guessed):
+    if(check_valid_input(letter_guessed, old_letters_guessed)):
+        old_letters_guessed.append(letter_guessed)
+        print(old_letters_guessed)
+        return True
+    else:
+        print("X\n"+'guessed letters until now :')
+        print( ' ='+'='*len(old_letters_guessed)*5+'\n|| '+' -> '.join(sorted(old_letters_guessed))+' ||\n'+' ='+'='*5*len(old_letters_guessed))
+        return False
+            		
 HANGMAN_ASCII_ART = """
    _    _
   | |  | |
@@ -31,10 +45,13 @@ HANGMAN_ASCII_ART = """
                        __/ |
                       |___/
  """
-MAX_TRIES=random.randint(5, 10)
+MAX_TRIES = random.randint(5, 10)
+letters_guessed = ['b','a','z','w','d','h']
 print(HANGMAN_ASCII_ART, MAX_TRIES)
 letter_guessed_by_player = input("Please Guess a letter: ")
-legail = is_valid_input(letter_guessed_by_player)
+letter_guessed_by_player_lowerLitr = letter_guessed_by_player.lower()
+print(letter_guessed_by_player_lowerLitr)
+legail = try_update_letter_guessed(letter_guessed_by_player_lowerLitr,letters_guessed)
 print(legail)
 word_typed_by_player = input("Please enter a word: ")
 print("_ " * len(word_typed_by_player))
