b85e7f1f6a44d02da3fd6b8dda66fc65484b5a8a
amar amer <september06@users.noreply.github.com>
2020-01-11 02:26:50 +0200

    assigment changes

--- lecture 8/assignment 6/‏‏תיקיה חדשה/lec6.py
+++ lecture 8/assignment 6/‏‏תיקיה חדשה/lec6.py
@@ -3,6 +3,8 @@
 #Return arandom integer between a maximum and minimum number incloud
 import random
 import re
+from hangman_text import hangman_text
+from hangman_print import hangman_print
 
 def check_valid_input(letter_guessed, old_letters_guessed):
 #the second and thred condution
@@ -35,24 +37,30 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed):
         print( ' ='+'='*len(old_letters_guessed)*5+'\n|| '+' -> '.join(sorted(old_letters_guessed))+' ||\n'+' ='+'='*5*len(old_letters_guessed))
         return False
             		
-HANGMAN_ASCII_ART = """
-   _    _
-  | |  | |
-  | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
-  |  __  |/ _` | '_ \\/  _ `| '_ ` _ \\ / _ `| '_ \\
-  | |  | | (_| | | | | (_| | | | | | | (_| | | | |
-  |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                       __/ |
-                      |___/
- """
-MAX_TRIES = random.randint(5, 10)
-letters_guessed = ['b','a','z','w','d','h']
-print(HANGMAN_ASCII_ART, MAX_TRIES)
-letter_guessed_by_player = input("Please Guess a letter: ")
-letter_guessed_by_player_lowerLitr = letter_guessed_by_player.lower()
-print(letter_guessed_by_player_lowerLitr)
-legail = try_update_letter_guessed(letter_guessed_by_player_lowerLitr,letters_guessed)
-print(legail)
-word_typed_by_player = input("Please enter a word: ")
-print("_ " * len(word_typed_by_player))
-input("please press enter to exit")
+					
+def main():
+    hangman_printD = {
+        1: hangman_print.hangman_print7,
+        2: hangman_print.hangman_print7,
+        3: hangman_print.hangman_print7,
+        4: hangman_print.hangman_print7,
+        5: hangman_print.hangman_print7,
+        6: hangman_print.hangman_print7,
+        7: hangman_print.hangman_print7
+    }					
+    hangman_text.print_hangman()
+    MAX_TRIES = random.randint(5, 10)
+    letters_guessed = ['b','a','z','w','d','h']
+    print(MAX_TRIES)
+    letter_guessed_by_player = input("Please Guess a letter: ")
+    letter_guessed_by_player_lowerLitr = letter_guessed_by_player.lower()
+    print(letter_guessed_by_player_lowerLitr)
+    legail = try_update_letter_guessed(letter_guessed_by_player_lowerLitr,letters_guessed)
+    print(legail)
+    word_typed_by_player = input("Please enter a word: ")
+    print("_ " * len(word_typed_by_player))
+    input("please press enter to exit")
+
+	
+if __name__ == "__main__":
+    main()
---
+++ lecture 8/assignment 6/‏‏תיקיה חדשה/try.py
@@ -0,0 +1,15 @@
+
+from hangman_drow import hangman_drow
+import os,time
+os.system('cls') #linus-'clear'
+file_name_drow = [hangman_drow.hangman_drow1(),
+	                  hangman_drow.hangman_drow2(),
+					  hangman_drow.hangman_drow3(),
+					  hangman_drow.hangman_drow4(),
+					  hangman_drow.hangman_drow5(),
+					  hangman_drow.hangman_drow6()
+					  ]
+
+hangman_drow.hangman_drow2()
+
+input("please press enter to exit")
