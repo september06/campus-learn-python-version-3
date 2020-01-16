1d14ae5004ab050adc3ca3012398095cf4ecd2cd
amar amer <september06@users.noreply.github.com>
2020-01-17 00:01:00 +0200

    update game part

--- assignment for sub/assignment.py
+++ assignment for sub/assignment.py
@@ -3,6 +3,7 @@
 #Return arandom integer between a maximum and minimum number incloud
 import random
 import re
+import os
 from hangman_text import hangman_text
 from hangman_print import hangman_print
 
@@ -33,11 +34,41 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed):
         print(old_letters_guessed)
         return True
     else:
-        print("X\n"+'guessed letters until now :')
-        print( ' ='+'='*len(old_letters_guessed)*5+'\n|| '+' -> '.join(sorted(old_letters_guessed))+' ||\n'+' ='+'='*5*len(old_letters_guessed))
+        print("X\n" + 'guessed letters until now :')
+        print( ' =' + '='*len(old_letters_guessed)*5 + '\n|| ' + ' -> '.join(sorted(old_letters_guessed))+ ' ||\n' + ' =' + '='*5*len(old_letters_guessed))
         return False
             		
 					
+def show_hidden_word(secret_word, old_letters_guessed):
+    show_hidden = '_ ' * len(secret_word)
+    for i in range(len(secret_word)):
+       if secret_word[i] == " ":
+            show_hidden = show_hidden[:2*(i)] + '  ' + show_hidden[(i+1)*2:]
+            print(show_hidden)
+    for k in range(0,len(old_letters_guessed)):
+        for j in range(0,len(secret_word)):
+             if old_letters_guessed[k] in secret_word[j]:
+                temp_string = list(show_hidden)
+                print(temp_string)
+                temp_string[j*2] = old_letters_guessed[k]
+                show_hidden = "".join(temp_string)
+    return show_hidden 
+		    
+def check_win(secret_word, old_letters_guessed):
+    player_guess = show_hidden_word(secret_word, old_letters_guessed)		
+    if(player_guess.replace(" ", "") in secret_word.replace(" ", "")):
+        return True
+    print(player_guess)
+    print(secret_word)
+    return False
+	
+def print_function(word_path = 'c:', index_word = -1, MAX_TRIES = 7 ):
+    hangman_text.print_hangman()
+    print(MAX_TRIES)
+    print("Please Enter Docminet Path : " + word_path )
+    print("Please Enter index for word in file :  " + index_word)
+	
+
 def main():
     hangman_print_doll	= {
         1: hangman_print.hangman_print1,
@@ -52,18 +83,23 @@ def main():
     MAX_TRIES = 7 #random.randint(5, 10)
     letters_guessed = ['b','a','z','w','d','h']
     print(MAX_TRIES)
+    i=1
     word_path = input("Please Enter Docminet Path : ")
     index_word = input("Please Enter index for word in file :  ")
-    hangman_print_doll[1]()
-    letter_guessed_by_player = input("Please Guess a letter: ")
-    letter_guessed_by_player_lowerLitr = letter_guessed_by_player.lower()
-    print(letter_guessed_by_player_lowerLitr)
-    legail = try_update_letter_guessed(letter_guessed_by_player_lowerLitr,letters_guessed)
-    print(legail)
+    while (1):
+        hangman_print_doll[i]()
+        letter_guessed_by_player = input("Please Guess a letter: ")
+        letter_guessed_by_player_lowerLitr = letter_guessed_by_player.lower()
+        print(letter_guessed_by_player_lowerLitr)
+        legail = try_update_letter_guessed(letter_guessed_by_player_lowerLitr,letters_guessed)
+        print(legail)
+        i=i+1       
+        os.system('cls')
+        print_function(word_path, index_word,MAX_TRIES )
     word_typed_by_player = input("Please enter a word: ")
     print("_ " * len(word_typed_by_player))
     input("please press enter to exit")
-
+        
 	
 if __name__ == "__main__":
     main()
---
+++ assignment for sub/hangman_print.py
@@ -0,0 +1,118 @@
+#the Situations of the hangman :
+#first one :
+class hangman_print:
+    @staticmethod
+    def hangman_print1():
+        print("""
+            h==y====x
+            d y     
+            dy      
+            d
+            d
+            d
+            d 
+            d
+            d
+            d 
+         +++m+++ """)
+
+#second one :
+    @staticmethod
+    def hangman_print2():
+        print("""
+            h==y====x
+            d y     |
+            dy      |
+            d
+            d
+            d
+            d
+            d
+            d
+            d
+         +++m+++ """)
+
+#picture 3:
+    @staticmethod
+    def hangman_print3():
+        print("""
+            h==y====x
+            d y     |
+            dy      |
+            d       0
+            d
+            d
+            d
+            d
+            d
+            d 
+         +++m+++ """)
+
+#picture 4:
+    @staticmethod
+    def hangman_print4():
+        print("""
+            h==y====x
+            d y     |
+            dy      |
+            d      _0_
+            d      |.|
+            d      | |
+            d
+            d
+            d
+            d
+         +++m+++ """)
+
+#picture 5:
+    @staticmethod
+    def hangman_print5():
+        print("""
+            h==y====x
+            d y     |
+            dy      |
+            d      _0_
+            d     /|.|\\
+            d    / |_| \\
+            d
+            d
+            d
+            d
+         +++m+++ """)
+
+#picture 6:
+    @staticmethod
+    def hangman_print6():
+        print("""
+            h==y====x
+            d y     |
+            dy      |
+            d      _0_
+            d     /|.|\\
+            d    / |_| \\
+            d     /  
+            d    _|
+            d
+            d
+         +++m+++ """)
+
+#picture 7:
+    @staticmethod
+    def hangman_print7():
+        print("""
+            h==y====x
+            d y     |
+            dy      |
+            d      _0_
+            d     /|.|\\
+            d    / |_| \\
+            d     /   \\
+            d    _|   |_
+            d
+            d   
+         +++m+++ """)
+ 
+
+
+
+
